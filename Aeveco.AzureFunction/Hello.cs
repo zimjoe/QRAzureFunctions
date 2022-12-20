f3d88d6dfd3a4af898d4b63b708b6afd1ada0188
zimjoe <zimjoe@gmail.com>
2022-12-19 23:58:51 -0500

    enabled nullable and formatted a bit

--- Aeveco.AzureFunction/Hello.cs
+++ Aeveco.AzureFunction/Hello.cs
@@ -19,11 +19,12 @@ namespace Aeveco.AzureFunction
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+
+            // prefer the body or grab the name
+            string? name = data?.name ?? req.Query["name"];
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
