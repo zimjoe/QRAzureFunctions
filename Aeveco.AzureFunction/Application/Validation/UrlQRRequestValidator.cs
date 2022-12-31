e4ba7279368154968d2b0ffbbf512c71e5dc3e20
zimjoe <zimjoe@gmail.com>
2022-12-31 16:50:49 -0500

    Added request DTOs and validation - needs actual testing still

---
+++ Aeveco.AzureFunction/Application/Validation/UrlQRRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using Aeveco.AzureFunction.Application.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aeveco.AzureFunction.Application.Validation
+{
+    public class UrlQRRequestValidator:AbstractValidator<UrlQRRequest>
+    {
+        public UrlQRRequestValidator()
+        {
+            RuleFor(x => x.Url).NotEmpty().MinimumLength(10);
+        }
+    }
+}
--- Aeveco.AzureFunction/UrlQR.cs
+++ Aeveco.AzureFunction/UrlQR.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using QRCoder;
+using Aeveco.AzureFunction.Extensions;
+using Aeveco.AzureFunction.Application.Models;
+using Aeveco.AzureFunction.Application.Validation;
 
 namespace Aeveco.AzureFunction
 {
@@ -18,27 +21,33 @@ namespace Aeveco.AzureFunction
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("URL HTTP trigger function processed a request.");
+            // grab the request form
+            var form = await req.GetJsonBody<UrlQRRequest, UrlQRRequestValidator>();
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            // prefer the body or grab the name
-            string? urlValue = data?.url ?? req.Query["url"];
-
-            if (string.IsNullOrEmpty(urlValue))
+            if (!form.IsValid)
             {
-                return new BadRequestObjectResult("Please supply a url to encode");
+                log.LogInformation("Invalid form data.");
+                return form.ToBadRequest();
             }
 
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic? data = JsonConvert.DeserializeObject(requestBody);
+            //// prefer the body or grab the name
+            //string? urlValue = data?.url ?? req.Query["url"];
 
-            PayloadGenerator.Url generator = new(urlValue);
+            //if (string.IsNullOrEmpty(urlValue))
+            //{
+            //    return new BadRequestObjectResult("Please supply a url to encode");
+            //}
+
+            PayloadGenerator.Url generator = new(form.Value?.Url);
+
             string payload = generator.ToString();
 
             QRCodeGenerator qrGenerator = new();
             QRCodeData qrCodeData = qrGenerator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
             PngByteQRCode qrCode = new(qrCodeData);
             var qrCodeAsPng = qrCode.GetGraphic(20);
-
 
             return new FileContentResult(qrCodeAsPng, "image/png");
         }
